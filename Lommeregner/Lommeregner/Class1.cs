d5d0bcdfb8a530a6e183f640dee613fa59db8932
Sandra Flindt <skfl62365@edu.ucl.dk>
2024-02-29 12:35:56 +0100

    Added all methods

--- Lommeregner/Lommeregner/Class1.cs
+++ Lommeregner/Lommeregner/Class1.cs
@@ -13,6 +13,18 @@ namespace Calculator
         {
             return x + y; 
         }
+        public int Subtract(int x, int y) 
+        {
+            return x - y;
+        }
+        public int Multiply(int x, int y) 
+        {
+            return x * y;
+        }
+        public int Divide(int x, int y) 
+        {
+            return x / y;
+        }
     }
 
 }
--- Lommeregner/Lommeregner/Program.cs
+++ Lommeregner/Lommeregner/Program.cs
@@ -3,11 +3,19 @@ namespace Lommeregner
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void TestAllMethods()
         {
             Calculator.Calculator MyCalculator = new Calculator.Calculator();
             int result = MyCalculator.Add(2,2);
-            Console.WriteLine("Result is " + result);    
+            Console.WriteLine("Addition is " + result);  
+            Console.WriteLine("Subtract is " +  MyCalculator.Subtract(3,2));
+            Console.WriteLine("Multiply is " + MyCalculator.Multiply(3, 3));
+            Console.WriteLine("Divition is " + MyCalculator.Divide(10, 2));
+            
+        }
+        static void Main(string[] args)
+        {
+            TestAllMethods();
         }   
     }
 }
