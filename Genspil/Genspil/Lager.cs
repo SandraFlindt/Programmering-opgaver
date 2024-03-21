7e5f1bfe07b7cbba5406597afd51a891d019db04
Sandra Flindt <skfl62365@edu.ucl.dk>
2024-03-21 15:14:28 +0100

    projekt genspil første commit

---
+++ Genspil/Genspil/Lager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genspil
+{
+    internal class Lager
+    {
+        public string spilNavn;
+       
+        public Lager(string spilNavn)
+        {
+            this.spilNavn = spilNavn;
+        }
+    }
+}
---
+++ Genspil/Genspil/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Genspil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         List<String> SpilListe = new List<String>();
+
+         Lager myLager = new Lager("Ludo");
+
+       
+            SpilListe.Add(myLager.spilNavn);
+
+            Console.WriteLine(myLager.spilNavn);
+
+        }
+    }
+}
